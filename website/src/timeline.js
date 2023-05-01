b14a198e89ee541cd819cf1df4ca7e3b1b45de01
hanaCasey <hannah.casey@epfl.ch>
2023-05-01 15:31:30 +0200

    images working

--- website/src/timeline.js
+++ website/src/timeline.js
@@ -1,8 +1,10 @@
 
 // Get the width of the Bootstrap container
-var containerWidth = d3.select(".container").node().getBoundingClientRect().width;
+var containerWidth = d3.select(".scrollable-right").node().getBoundingClientRect().width;
 var height = 400;
-const radius = 5;
+const radius = 10;
+const enlarged = radius * 8;
+
 
 d3.csv("./src/data/books.csv").then( function(data) {
     
@@ -31,52 +33,79 @@ var xScale = d3.scaleTime()
    .domain([new Date(1440, 0, 1), new Date()])
    .range([50, containerWidth - 50]);
 
- // Add circles
- var circles = svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d) { console.log(d.title);return xScale(d.date);})
-    .attr("cy", height / 2)
-    .attr("r", radius)
-    .on("click", function(e, d) {
+// Add the image pattern definitions
+var defs = svg.append("defs");
+data.forEach(function(d) {
+  defs.append("pattern")
+    //.attr("id", d.id) for later
+    .attr("id", "testImage-" + d.book_id) // using unique id for each pattern
+    .attr("patternContentUnits", "objectBoundingBox")
+    .attr("width", 1)
+    .attr("height", 1)
+    .append("image")
 
-        e.stopPropagation(); // Prevent click event from bubbling up to the SVG element
-        var circle = d3.select(this);
-        var tooltipWidth = 100;
-        var tooltipHeight = 50;
-        var tooltipX = circle.attr("cx") - tooltipWidth / 2;
-        var tooltipY = circle.attr("cy") - tooltipHeight - radius - 5;
-        tooltip.style("left", tooltipX + "px")
-                .style("top", tooltipY + "px")
-                .style("visibility", "visible")
-                .append("rect")
-                .attr("x", 0)
-                .attr("y", 0)
-                .attr("width", tooltipWidth)
-                .attr("height", tooltipHeight)
-                .attr("fill", "white")
-                .attr("stroke", "black")
-                .attr("rx", 5)
-                .attr("ry", 5)
-                .on("click", function() {
-                    tooltip.style("visibility", "hidden");
-                });
-        tooltip.append("text")
-                .text(d.author + " (" + d.year + ")")
-                .attr("x", tooltipWidth / 2)
-                .attr("y", tooltipHeight / 2)
-                .style("text-anchor", "middle")
-                .style("alignment-baseline", "middle");
-    })
-    .on("mouseover", function(d) {
-    d3.select(this).attr("r", radius * 1.5);
-    })
-    .on("mouseout", function(d) {
-    d3.select(this).attr("r", radius);
+    .attr("xlink:href", "src/img/example.png")
+    .attr("preserveAspectRatio", "xMidYMid meet")
+    .attr("width", 1)
+    .attr("height", 1)
+});
+ // Add circles
+var circles = svg.selectAll("circle")
+.data(data)
+.enter()
+.append("circle")
+.attr("fill", "black")
+.attr("cx", function(d) {return xScale(d.date);})
+.attr("cy", height / 2)
+.attr("r", radius)
+
+
+.on("mouseover", function(d) {
+    d3.select(this)
+    .attr("r", enlarged)
+    .attr("fill", function(d) { return "url(#testImage-" + d.book_id + ")" })
+    .attr("z-index", 1)
+})
+.on("mouseout", function(d) {
+    d3.select(this).attr("r", radius)
+    .attr("fill", "black")
+    .attr("z-index", null)
+        
     tooltip.style("visibility", "hidden");
-    });
-    
+    tooltip.select("text").remove();
+})
+
+.on("click", function(e, d) {
+
+    e.stopPropagation(); // Prevent click event from bubbling up to the SVG element
+    var circle = d3.select(this);
+    var tooltipWidth = 100;
+    var tooltipHeight = 50;
+    var tooltipX = (containerWidth - tooltipWidth) / 2;
+    var tooltipY = height + radius + 5; // position below the timeline
+    tooltip.style("left", tooltipX + "px")
+            .style("top", tooltipY + "px")
+            .style("visibility", "visible")
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", tooltipWidth)
+            .attr("height", tooltipHeight)
+            .attr("fill", "white")
+            .attr("stroke", "black")
+            .attr("rx", 5)
+            .attr("ry", 5)
+
+    // Remove existing text before adding new text
+    tooltip.selectAll("text").remove();
+    tooltip.append("text")
+            .text(d.author + " (" + d.year + ")")
+            .attr("x", tooltipWidth / 2)
+            .attr("y", tooltipHeight / 2)
+            .style("text-anchor", "middle")
+            .style("alignment-baseline", "middle");
+});
+
 // Add axes and labels
 var xAxis = d3.axisBottom(xScale);
 svg.append("g")
@@ -87,6 +116,8 @@ svg.append("text")
     .attr("y", 90)
     .style("text-anchor", "middle")
     .text("Viz 1");
+svg.append("img")
+    .attr("background-image", "url(#testImage")
 
 
   });
