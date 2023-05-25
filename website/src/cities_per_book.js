b4f4c2e54c4d8fc361964b104e6752afec55136e
Zoe Jeandupeux <zoe.jeandupeux@epfl.ch>
2023-05-25 16:49:59 +0200

    3rd viz first draft

--- website/src/cities_per_book.js
+++ website/src/cities_per_book.js
@@ -5,13 +5,12 @@ var width = d3
 
 var height = 400;
 var margin = { top: 20, right: 20, bottom: 40, left: 40 };
-var chartWidth = width - margin.left - margin.right;
-var chartHeight = height - margin.top - margin.bottom;
   
 
 function createCitiesViz(bookid) {
 
     console.log("Cities load");
+    
   
     // Load the selected book's data
     d3.json("./src/data/valence_arousal.json").then(function (data) {
@@ -22,9 +21,6 @@ function createCitiesViz(bookid) {
             citiesdata[i]=data[bookid][i];
 
         }
-        console.log(citiesdata);
-        
-        
 
         //document.getElementById("output").innerHTML=blablabla;//citiesdata[0].toString();
   
@@ -40,7 +36,7 @@ function createCitiesViz(bookid) {
 
         svg.append("g")
           .attr("class", "x-axis")
-          .attr("transform", "translate(0," + height + ")")
+          .attr("transform", "translate(0," + height/2 + ")")
           .call(d3.axisBottom(XScale));
                         
 
@@ -50,19 +46,83 @@ function createCitiesViz(bookid) {
 
         svg.append("g")
           .attr("class", "y-axis")
+          .attr("transform", "translate(" + width/2 + ", 0)")
           .call(d3.axisLeft(YScale))
+
+        // Add X axis label:
+        svg.append("text")
+          .attr("text-anchor", "middle")
+          .attr("x", width - 30)
+          .attr("y", height/2 + 35)
+          .text("Arousal")
+          .style("font-size", "16px")
+
+        // Y axis label:
+        svg.append("text")
+          .attr("text-anchor", "middle")
+          .attr("transform", "rotate(-90)")
+          .attr("y", -margin.left + width/2)
+          .attr("x", -margin.top)
+          .text("Valence")
+          .style("font-size", "16px")
         
-        // Plotting the points
-        svg.append()
-          .selectAll("dot")
-          .data(citiesdata)
-          .enter()
-          .append("circle")
-            //.attr("class", "dot")
-            .attr("cx", d => {return d.city_arousal})  // Example: Using age as the x-coordinate
-            .attr("cy", d => {return d.city_valence})  // Example: Using the index as the y-coordinate
-            .attr("r", 5)
-            .style("fill", primaryColor)
+
+        // Add tooltip
+        var tooltip_cityemo = d3
+           .select("body")
+           .append("div")
+           .attr("class", "emotion-tooltip")
+           .style("visibility", "hidden")
+
+        var mouseover = function(d) {
+            tooltip_cityemo
+              .style("visibility", "visible")
+            d3.select(this)
+              .style("stroke", "black")
+              .style("opacity", 1)
+          }
+        var mousemove = function(event, d) {
+            console.log(d.city);
+            console.log(d.city_valence);
+            tooltip_cityemo.style("visibility", "visible");
+
+
+            tooltip_cityemo
+              .html("City: " + d.city + "<br> Valence: " + d.city_valence.toFixed(2)
+               + "<br> Arousal: "+ d.city_arousal.toFixed(2) + "<br> Emotion: "+ d.emotion)
+              .style("left", event.pageX + 90 + "px")
+              .style("top", event.pageY + "px")
+          }
+        var mouseleave = function(d) {
+            tooltip_cityemo
+              .transition()
+              .duration(200)
+              .style("visibility", "hidden")
+            d3.select(this)
+              .style("stroke", "none")
+              .style("opacity", 1)
+          }
+
+         // Plotting the points
+        svg
+         .selectAll(".dot")
+         .data(citiesdata)
+         .enter()
+         .append("circle")
+           .attr("class", "dot")
+           .attr("cx", d => XScale(d.city_arousal)) 
+           .attr("cy", d => YScale(d.city_valence))  
+           .attr("r", 5)
+           .style("fill", primaryColor)
+           .style("stroke", "none")
+           .style("opacity", 1)
+         .on("mouseover", mouseover)
+         .on("mousemove", mousemove)
+         .on("mouseleave", mouseleave)
+         
+        
+
+        
 
 
   
