ced1542c9c69ccc946c4f50e4133bdb0b09f3b53
Zoe Jeandupeux <zoe.jeandupeux@epfl.ch>
2023-06-03 12:46:16 +0200

    a tiny bit interactive

--- docs/src/cities_per_book.js
+++ docs/src/cities_per_book.js
@@ -7,6 +7,14 @@ var margin = { top: 20, right: 20, bottom: 40, left: 40 };
 const chartWidth = width - margin.left - margin.right;
 const chartHeight = height - margin.top - margin.bottom;
 
+function whenDocumentLoaded(action) {
+	if (document.readyState === "loading") {
+    	document.addEventListener("DOMContentLoaded", action);
+	} else {  // `DOMContentLoaded` already fired
+		action();
+	}
+}
+
 
 function checkFileExists(fileUrl) {
   return fetch(fileUrl, { method: 'HEAD' })
@@ -65,19 +73,21 @@ function createCitiesViz(bookid) {
       Promise.all(fetchPromises)
         .then(() => {
           
-      
+        let dots;
+        let svg;
         createEmotionCities(citiesdata);
 
-        // Fetch the coordinates.json file and cache the data
-        d3.json("./src/data/coordinates.json").then(function (data) {
-          const cachedCoordinates = data;
-          const map = L.map('cities-map').setView([41.8933203, 12.4829321], 6);
+        const map = L.map('cities-map').setView([41.8933203, 12.4829321], 6);
 
           L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
+            maxZoom: 10,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           }).addTo(map);
 
+        // Fetch the coordinates.json file and cache the data
+        d3.json("./src/data/coordinates.json").then(function (data) {
+          let cachedCoordinates = data;
+          
           createMap(citiesdata, map, cachedCoordinates);
         });
 
@@ -97,12 +107,9 @@ function color(img){
   }
 }
 
-function rescale (d) {
+function rescale (d, rescaled_max=25, rescaled_min=5) {
   const max = 450; //max seems to be 420 for Rome somewhere
   const min = 1;
-  const rescaled_max = 25;
-  const rescaled_min = 5;
-
   const normalized = (d - min) / (max - min);
   return normalized * (rescaled_max - rescaled_min) + rescaled_min;
 
@@ -117,11 +124,12 @@ function createMap(citiesdata, map, cached){
     }
   });
 
-  for (index in citiesdata){
-    city= citiesdata[index].city;
+
+  for (let index in citiesdata){
+    let city= citiesdata[index].city;
     if (cached && cached.hasOwnProperty(city)){
-      lat=cached[city]['lat'];
-      lon=cached[city]['lon'];
+      let lat=cached[city]['lat'];
+      let lon=cached[city]['lon'];
       let frequency=citiesdata[index].freq;
       //const marker = L.marker([lat,lon]).addTo(map);
 
@@ -129,17 +137,26 @@ function createMap(citiesdata, map, cached){
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 5000*rescale(frequency) 
+        radius: 5000*rescale(frequency, 30, 2) 
       }).addTo(map);
 
       // Bind popup on hover
       circle.on('mouseover', function (e) {
         this.openPopup();
+        console.log(city);
+        const selectedName = city;
+
+        // Highlight the corresponding point in the second graph
+        dots.filter(d => d.city === selectedName)
+           .style('fill', '#f03')
+           .style('opacity', 0.8);
       });
 
       // Hide popup on mouseout
       circle.on('mouseout', function (e) {
         this.closePopup();
+        dots.filter(d => d.city === city)
+           .style('fill', d => color(d.img));
       });
 
       if (frequency===1){
@@ -156,41 +173,41 @@ function createMap(citiesdata, map, cached){
 }
 
 function createEmotionCities(citiesdata){
-  const svg = d3.select("#cities-scatter")
+  svg = d3.select("#cities-scatter")
         .attr("width", width )
         .attr("height", height )
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-       const XScale = d3.scaleLinear()
+  const XScale = d3.scaleLinear()
         .domain([-1, 1])
         .range([0, chartWidth]);
 
-       svg.append("g")
+  svg.append("g")
         .attr("class", "x-axis")
         .attr("transform", "translate(0," + chartHeight/2 + ")")
         .call(d3.axisBottom(XScale));
 
 
-       const YScale = d3.scaleLinear()
+  const YScale = d3.scaleLinear()
         .domain([-1, 1])
         .range([chartHeight, 0]);
 
-       svg.append("g")
+  svg.append("g")
         .attr("class", "y-axis")
         .attr("transform", "translate(" + chartWidth / 2 + ", 0)")
         .call(d3.axisLeft(YScale))
 
-       // Add X axis label:
-       svg.append("text")
+  // Add X axis label:
+  svg.append("text")
         .attr("text-anchor", "middle")
         .attr("x", chartWidth - 30)
         .attr("y", chartHeight / 2 + 35)
         .text("Arousal")
         .style("font-size", "16px")
 
-       // Y axis label:
-       svg.append("text")
+  // Y axis label:
+  svg.append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "rotate(-90)")
         .attr("y", -margin.left + chartWidth / 2)
@@ -199,8 +216,8 @@ function createEmotionCities(citiesdata){
         .style("font-size", "16px")
 
 
-       // Add tooltip
-       const tooltip_cityemo = d3
+  // Add tooltip
+  let tooltip_cityemo = d3
         .select("body")
         .append("div")
         .attr("class", "emotion-tooltip")
@@ -208,14 +225,14 @@ function createEmotionCities(citiesdata){
         .style("position", "absolute")
         .style("z-index", 99999);
 
-       const mouseover = function (d) {
+  const mouseover = function (d) {
         tooltip_cityemo
           .style("visibility", "visible")
         d3.select(this)
           .style("stroke", "black")
           .style("opacity", 0.8)
-       }
-       const mousemove = function (event, d) {
+  }
+  const mousemove = function (event, d) {
 
         tooltip_cityemo.style("visibility", "visible");
         const textcity = d.city.toString();
@@ -226,8 +243,8 @@ function createEmotionCities(citiesdata){
             + "<br> Cited: " + d.freq + " times")
           .style("left", event.pageX + 10 + "px")
           .style("top", event.pageY - 90 + "px");
-       }
-       const mouseleave = function (d) {
+  }
+  const mouseleave = function (d) {
         tooltip_cityemo
           .transition()
           .duration(200)
@@ -236,11 +253,11 @@ function createEmotionCities(citiesdata){
         d3.select(this)
           .style("stroke", "none")
           .style("opacity", 0.8)
-       }
+  }
 
 
-       // Plotting the points
-       svg
+  // Plotting the points
+  dots=svg
         .selectAll(".dot")
         .data(citiesdata)
         .enter()
@@ -291,6 +308,5 @@ function createEmotionCities(citiesdata){
         })
 
 }
-
 
 
